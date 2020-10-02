97e9068c6afa052ff3f93e5770e31b24c0bc8699
github username <56710845+m-delarosa@users.noreply.github.com>
2020-10-01 20:35:16 -0600

    Add NewYearsChaos.js

--- interview-prep-kit/Arrays/NewYearChaos.js
+++ interview-prep-kit/Arrays/NewYearChaos.js
@@ -14,19 +14,63 @@
 const q = [2, 1, 5, 3, 4]
 const chaoticQ = [2, 5, 1, 3, 4]
 
-function minimumBribes(q) {
-    let bribes = 0
+// function minimumBribes(q) {
+//     let bribes = 0
 
-    for (let i = 0; i < q.length; i++) {
-        if (q[i] > [i + 1]) {
-            if (q[i] - [i + 1] === 1) bribes++
-            else if (q[i] - [i + 1] === 2) bribes += 2
-            else if (q[i] - [i + 1] > 2) return console.log("Too chaotic")
+//     for (let i = 0; i < q.length; i++) {
+//         if (q[i] > [i + 1]) {
+//             if (q[i] - [i + 1] === 1) bribes++
+//             else if (q[i] - [i + 1] === 2) bribes += 2
+//             else if (q[i] - [i + 1] > 2) return console.log("Too chaotic")
+//         }
+//     }
+
+//     console.log(bribes)
+// }
+
+//Dooesn't work
+// function minimumBribes(q) {
+//     let bribes = 0
+
+//     for (let i = 0; i < q.length; i++) {
+//         if (q[i] > [i + 1]) {
+//             if (q[i] - q[i + 1] === 1) bribes++
+//             else if (q[i] - q[i + 1] === 2) bribes += 2
+//             else if (q[i] - q[i + 1] > 2) return console.log("Too chaotic")
+//         }
+//     }
+
+//     console.log(bribes)
+// }
+
+//found solution
+function minimumBribes(q) {
+    let total = 0
+
+    for (let current_pos = 0; current_pos < q.length; current_pos++) {
+        //Getting original position starting at 0, so 1 at q[0] will equal 0
+        const original_pos = q[current_pos] - 1
+        // diff = ho wmaany positions a person has moved. If 1 at q[0], 0-0=0 positions movied,
+        // if 2 at q[0] 1-0=1 position moved.
+        const diff = original_pos - current_pos
+
+        //if person had moved more than 2 spots, than impossible
+        if (diff > 2) return console.log("Too chaotic")
+
+        //If person has not moved up at all or has actually moved down.
+        if (diff <= 0) {
+            //check each person starting from one person ahead of original pos up until current position
+            for (let i = Math.max(0, original_pos - 1); i < current_pos; i++) {
+                const start_pos_of_forward_person = q[i] - 1
+                if (start_pos_of_forward_person > original_pos) {
+                    total++
+                }
+            }
         }
     }
-
-    console.log(bribes)
+    console.log(total)
 }
+
 
 console.log("q", minimumBribes(q))
 console.log("chaoticQ", minimumBribes(chaoticQ))
